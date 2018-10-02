aa6385eaa5969ce4633f04eab750d55aacf03148
Farhan Fauzan Abdurrahman <43526521+farhanfauzanabdurrahman1128@users.noreply.github.com>
2018-10-02 16:51:16 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -5,7 +5,31 @@
 </head>
 <body>
 
-
+<form action="prosreg.php" method="POST">
+	<table>
+		<tr>
+			<td><b>Masukan Data</b></td>
+		</tr>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="text" name="nim" minlength="10" maxlength="10"></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" maxlength="25"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td><input type="email" name="email" value="@gmail.com"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="Daftar"></td>
+		</tr>
+	</table>
+</form>
 
 </body>
 </html>
