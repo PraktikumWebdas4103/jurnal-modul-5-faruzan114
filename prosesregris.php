1496a7eaa6bffdae7a9d934c6019f83e4247e6ea
Farhan Fauzan Abdurrahman <43526521+farhanfauzanabdurrahman1128@users.noreply.github.com>
2018-10-02 16:50:28 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,38 @@
 <?php
+$host = "localhost"; //host server
+$user = "root"; //user login phpmyadmin 
+$pass = ""; //user login phpmyadmin
+$db = "registrasi"; //nama database
 
+if (isset($_POST['submit'])) {
+			$nim = $_REQUEST['nim'];
+			$nama = $_REQUEST['nama'];
+			$email = $_REQUEST['email'];
+			$conn = mysqli_connect($host, $user, $pass,$db);
+			if ($conn==false) {
+				if ($confpassword != $password) {
+					die("Gagal Boss");
+				}
+			}
+  			
+if ($_POST['submit']) {
+			$nim = $_REQUEST['nim'];
+			$nama = $_REQUEST['nama'];
+			$email = $_REQUEST['email'];
+
+	if (empty($nim) || empty($nama) || empty($email)) {
+				echo "Data Harus Diisi";
+			}
+	else{
+					$mysqli = "INSERT INTO registrasi (nim, nama, email) 
+					  VALUES ('$nim', '$nama', '$email')";
+			$result = mysqli_query($conn, $mysqli);
+			
+			echo "Input Berhasil";
+	}
+		}
+
+				
+}
 
 ?>
