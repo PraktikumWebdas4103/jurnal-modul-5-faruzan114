89808c58a718c9ed429fcceceff87e2ebb542632
Farhan Fauzan Abdurrahman <43526521+farhanfauzanabdurrahman1128@users.noreply.github.com>
2018-10-02 16:54:03 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+<body>
+	<center>
+    Komentar<br>
+	<form method="POST">
+		<table>
+			<tr><input type="text" name="komentar" placeholder="komentar" minlength="5" required></tr><br><br>
+			<tr><input type="submit" name="submit" value="hitung jumlah kata"></tr><br><br>
+		</table>
+	</form>
+</body>
+</html>
+	</center>
+</form>
+
 <?php
+	if(isset($_POST['submit'])){
+		$komentar = $_POST['komentar'];
+		$hasil = str_word_count($komentar);
+			echo "Jumlah kata pada komentar  adalah : $hasil";
+	}
 
 ?>
